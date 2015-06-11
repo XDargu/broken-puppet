b766c9605a82fb1332c83a98d6c94b7891a9c229
XDargu <xdargu@gmail.com>
2015-06-11 06:21:37 +0200

    Pruebas de romper cuerdas

--- engine/engine/components/comp_rope.cpp
+++ engine/engine/components/comp_rope.cpp
@@ -2,6 +2,9 @@
 #include "comp_rope.h"
 #include "comp_distance_joint.h"
 #include "comp_transform.h"
+#include "entity_manager.h"
+#include "rope_manager.h"
+#include "physics_manager.h"
 
 void TCompRope::setPositions(CHandle the_transform_1, XMVECTOR the_pos_2) {
 
@@ -59,64 +62,68 @@ void TCompRope::fixedUpdate(float elapsed) {
 
 		//pos_2 = trans_2->position;
 	}
-	// If the joint distance exceeds the max distance, the destroy the entity
-	/*float dist = ((TCompDistanceJoint*)joint)->joint->getDistance();
-	if (dist > max_distance * max_distance) {
-	CEntityManager::get().remove(CHandle(this).getOwner());
+
+
+	// Rope collisions
+	CHandle e1 = CHandle();
+	CHandle e2 = CHandle();
+	CHandle e3 = CEntityManager::get().getByName("Player");
+
+	if (transform_1.isValid()) {
+		e1 = transform_1.getOwner();
+	}
+	if (transform_2.isValid()) {
+		e2 = transform_2.getOwner();
 	}
 
-	// Raycast
-	TCompDistanceJoint* m_joint = (TCompDistanceJoint*)joint;
-	physx::PxRigidActor* a1 = nullptr;
-	physx::PxRigidActor* a2 = nullptr;
+	XMVECTOR initialPos = pos_1;
+	XMVECTOR dir = XMVector3Normalize(pos_2 - pos_1);
+	float length = V3DISTANCE(pos_2, pos_1);
 
-	m_joint->joint->getActors(a1, a2);
+	PxRaycastBuffer buf;
 
-	XMVECTOR offset_pos1 = Physics.PxVec3ToXMVECTOR(m_joint->joint->getLocalPose(physx::PxJointActorIndex::eACTOR0).p);
-	XMVECTOR offset_pos2 = Physics.PxVec3ToXMVECTOR(m_joint->joint->getLocalPose(physx::PxJointActorIndex::eACTOR1).p);
-
-	XMVECTOR pos1 = Physics.PxVec3ToXMVECTOR(a1->getGlobalPose().p);
-	XMVECTOR pos2 = Physics.PxVec3ToXMVECTOR(a2->getGlobalPose().p);
-
-	XMVECTOR rot1 = Physics.PxQuatToXMVECTOR(a1->getGlobalPose().q);
-	XMVECTOR rot2 = Physics.PxQuatToXMVECTOR(a2->getGlobalPose().q);
-
-	XMVECTOR offset_rotado_1 = XMVector3Rotate(offset_pos1, rot1);
-	XMVECTOR offset_rotado_2 = XMVector3Rotate(offset_pos2, rot2);
-
-	//   RECREATE ROPE
-	// Obtener el punto en coordenadas de mundo = Offset * rotación + posición
-	XMVECTOR initialPos = pos1 + offset_rotado_1;
-	XMVECTOR finalPos = pos2 + offset_rotado_2;
-
-	physx::PxVec3 dir = Physics.XMVECTORToPxVec3(finalPos - initialPos).getNormalized();
-
-	physx::PxRaycastBuffer buf;
-
-	Physics.raycastAll(Physics.XMVECTORToPxVec3(initialPos), dir, sqrt(dist), buf);
+	Physics.raycastAll(initialPos, dir, length, buf);
 	float force_s = 100;
 
+	bool colliding = false;
 	for (int i = 0; i < buf.nbTouches; i++)
 	{
-	if (buf.touches[i].actor != a1 && buf.touches[i].actor != a2) {
-	if (buf.touches[i].actor->isRigidBody()) {
-	if (((physx::PxRigidBody*)buf.touches[i].actor)->getMass() < 100) {
+		CHandle e(buf.touches[i].actor->userData);
+		if (!(e == e1) && !(e == e2) && !(e == e3)) {
+			if (buf.touches[i].actor->isRigidBody()) {
+				/*if (((physx::PxRigidBody*)buf.touches[i].actor)->getMass() < 100) {
 
-	physx::PxRigidBody* body = ((physx::PxRigidBody*)buf.touches[i].actor);
+					physx::PxRigidBody* body = ((physx::PxRigidBody*)buf.touches[i].actor);
 
-	physx::PxVec3 force = (body->getGlobalPose().p - buf.touches[i].position).getNormalized();
+					physx::PxVec3 force = (body->getGlobalPose().p - buf.touches[i].position).getNormalized();
 
-	physx::PxVec3 velocity = physx::PxRigidBodyExt::getVelocityAtPos(*body, buf.touches[i].position);
+					physx::PxVec3 velocity = physx::PxRigidBodyExt::getVelocityAtPos(*body, buf.touches[i].position);
 
-	physx::PxRigidBodyExt::addForceAtPos(*body, -velocity, buf.touches[i].position, physx::PxForceMode::eVELOCITY_CHANGE);
-	//((physx::PxRigidBody*)buf.touches[i].actor)->addForce(force * force_s, physx::PxForceMode::eIMPULSE);
+					physx::PxRigidBodyExt::addForceAtPos(*body, -velocity, buf.touches[i].position, physx::PxForceMode::eVELOCITY_CHANGE);
+					//((physx::PxRigidBody*)buf.touches[i].actor)->addForce(force * force_s, physx::PxForceMode::eIMPULSE);
+				}
+				else
+				{
+					int a = 2;
+					//CEntityManager::get().remove(CHandle(this).getOwner());
+				}*/
+			}
+			else
+			{
+				// Remove the string
+				remove_counter += elapsed;
+				colliding = true;
+				if (remove_counter > 0.5f) {
+					CRope_manager::get().removeString(CHandle(this));
+					int b = 2;
+				}
+			}
+		}
 	}
-	else
-	{
-	CEntityManager::get().remove(CHandle(this).getOwner());
-	}
-	}
-	}
-	}*/
+
+	if (!colliding)
+		remove_counter = 0;
+
+	
 
 }
