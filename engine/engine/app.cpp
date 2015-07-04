5c0bc3e4c3f1d8ae4c63be96c5b7ad1623956043
Dani G <thedanitime@gmail.com>
2015-07-04 18:43:15 +0200

    Arreglado teletransporte de player al morir.

--- engine/engine/app.cpp
+++ engine/engine/app.cpp
@@ -363,7 +363,7 @@ bool CApp::create() {
 	//loadScene("data/scenes/escena_2_ms3.xml");
 	//loadScene("data/scenes/scene_volum_light.xml");
 	//loadScene("data/scenes/viewer.xml");
-	loadScene("data/scenes/my_file.xml");
+	//loadScene("data/scenes/my_file.xml");
 	//loadScene("data/scenes/desvan_test.xml");
 	//loadScene("data/scenes/lightmap_test.xml");
 	//loadScene("data/scenes/anim_test.xml");
@@ -371,7 +371,7 @@ bool CApp::create() {
 
 
 	// XML Pruebas
-	//loadScene("data/scenes/scene_1.xml");
+	loadScene("data/scenes/scene_1.xml");
 	//loadScene("data/scenes/scene_2.xml");
 
 	//loadScene("data/scenes/scene_3.xml");
